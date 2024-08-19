73485f1e32e15f942a1e8bbe8cca4a202d4c44e0
Lobanov Vasiliy <lobanov.v@biostyle.biz>
2024-08-19 16:37:14 +0300

    Del for comments

--- essockets.h
+++ essockets.h
@@ -1,5 +1,4 @@
 #pragma once
-// люйпняя менаундхлши б WINDOWS 
 #define WIN32_LEAN_AND_MEAN 
 
 #include <iostream>
@@ -7,7 +6,6 @@
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 
-// ондйкчвемхе DLL ахакхнрейх WinSock2 
 #pragma comment(lib, "Ws2_32.lib") 
 
 namespace web {
@@ -15,14 +13,14 @@ namespace web {
     class server {
 
         WSADATA wsaDta;
-        ADDRINFO hints;                                      // срнвмемхъ янйерю
-        ADDRINFO* addrResult = NULL;                         // ярпсйрспю дкъ нрберю getaddrinfo()
-        SOCKET ClientSocket = INVALID_SOCKET;                // янгдюмхе янйерю х хмхжхюкхгюжхъ,         INVALID_SOCKET - мебепмши янйер 
-        SOCKET ListenSocket = INVALID_SOCKET;                // яксыючыхи янйер
+        ADDRINFO hints;                                      
+        ADDRINFO* addrResult = NULL;                         
+        SOCKET ClientSocket = INVALID_SOCKET;                
+        SOCKET ListenSocket = INVALID_SOCKET;                
 
         int result = 0;
-        const char* sendBuffer = "Hello from Server";        // астеп дкъ нропюбйх яннаыемхъ мю яепбеп
-        char recvBuffer[512] = "";                           // астеп дкъ опх╗лю яннаыемхъ я яепбепю
+        const char* sendBuffer = "Hello from Server";        
+        char recvBuffer[512] = "";                           
 
 
 
@@ -31,11 +29,11 @@ namespace web {
         server(ADDRINFO* addrinfo) {
             
 
-            ZeroMemory(&hints, sizeof(hints));              // нвхярйю оюлърх ярпсйрспш
-            hints.ai_family = addrinfo->ai_family;         // бшанп яелеиярбю опнрнйнкнб,                   AF_INET     - IpV4
-            hints.ai_socktype = addrinfo->ai_socktype;    // бшанп онксвюелнцн янйерю,                      SOCK_STREAM - онрнйнбши янйер
-            hints.ai_protocol = addrinfo->ai_protocol;   // бшанп хяонкэгселнцн опнрнйнкю,                  IPPROTO_TCP - Tcp/IP
-            hints.ai_flags = addrinfo->ai_flags;        // онксвемхе хмтнплюжхх дкъ оюяяхбмни ярнпнмш       AI_PASSIVE  - оюяяхбмне онксвемхе
+            ZeroMemory(&hints, sizeof(hints));              
+            hints.ai_family = addrinfo->ai_family;                            
+            hints.ai_socktype = addrinfo->ai_socktype;    
+            hints.ai_protocol = addrinfo->ai_protocol;  
+            hints.ai_flags = addrinfo->ai_flags;        
 
 
 
@@ -46,7 +44,6 @@ namespace web {
             ReleaseResources();
         }
 
-        // нябнандхрэ пеяспяш
         int ReleaseResources() {
 
             closesocket(ClientSocket);
@@ -56,11 +53,8 @@ namespace web {
             return 0;
         }
 
-        // сярюмнбхрэ бепяхч янйернб
         int SetWSADATA() {
 
-            // оепедю╗л бепяхч янйернб WINDOWS: MAKEWORD(2, 2)
-            // х яяшкйс мю ярпсйрспс WsaData
             result = WSAStartup(MAKEWORD(2, 2), &wsaDta);
 
             if (result != 0)
@@ -73,14 +67,13 @@ namespace web {
             return 0;
         }
 
-        // опнбепъер мюкхвхе сдюк╗ммнцн сгкю б яерх, х ошрюеряъ онярпнхрэ й мелс осрэ
         int CheckSocket(PCSTR port) {
 
             result = getaddrinfo(NULL, port, &hints, &addrResult);
             if (result != 0)
             {
                 std::cout << "getaddrinfo() failed, with error: " << result << std::endl;
-                WSACleanup(); // б яксвюе теикю нвхыюел
+                WSACleanup();
 
                 return 1;
             }
@@ -88,7 +81,6 @@ namespace web {
             return 0;
         }
 
-        // янгдю╗р янйер
         int InitConnection() {
 
             ListenSocket = socket(addrResult->ai_family, addrResult->ai_socktype, addrResult->ai_protocol);
@@ -105,18 +97,12 @@ namespace web {
             return 0;
         }
 
-        // сярюмнбйю янйерю мю опняксьхбюмхе        
         int SetListening() {
 
-            result = bind(ListenSocket, addrResult->ai_addr, (int)addrResult->ai_addrlen); // опхмхлюер:
-            // янгдюммши янйер:         ConnectSocket
-            // юдпея дкъ янедхмемхъ:    addrResult->ai_addr
-            // дкхмю хлемх юдпеяю:      addrResult->ai_addrlen
+            result = bind(ListenSocket, addrResult->ai_addr, (int)addrResult->ai_addrlen);
 
-            // опнбепйю янедхмемхъ
             if (result == SOCKET_ERROR)
             {
-                // б яксвюе ньхайх янедхмемхъ нябнанфдюел пеяспяш
                 std::cout << "Binding socket failed" << std::endl;
                 ListenSocket = INVALID_SOCKET;
                 ReleaseResources();
@@ -127,13 +113,9 @@ namespace web {
             return 0;
         }
 
-        // сярюмюбкхбюел пефхл опняксьхбюмхъ янйерю
         int SetListeningMode() {
 
-            // listen() - ъбкъеряъ акнйхпсчыеи тсмйжхеи, фд╗р яннаыемхе акнйхпсъ онрнй
-            result = listen(ListenSocket, SOMAXCONN); // опхмхлюер:
-            // янйер дкъ опняксьхбюмхъ                  ListenSocket
-            // люйяхлюкэмне йнк-бн янедхмемхи           SOMAXCONN - люйяхлюкэмне йнк-бн
+            result = listen(ListenSocket, SOMAXCONN);
 
             if (result == SOCKET_ERROR)
             {
@@ -151,7 +133,6 @@ namespace web {
             return 0;
         }
 
-        // пюгпеьюел янедхмемхе х янгдю╗л йкхемряйхи янйер
         int AcceptConnection() {
 
             ClientSocket = accept(ListenSocket, NULL, NULL);
@@ -170,18 +151,14 @@ namespace web {
             return 0;
         }
 
-        // опхмхлюел яннаыемхъ нр сгкю
         int GetUpdates() {
 
             do
             {
-                // нвхыюел астеп дкъ опх╗лю
                 ZeroMemory(recvBuffer, 512);
 
-                result = recv(ClientSocket, recvBuffer, 512, 0); // опхмхлюер:
-                // янйер:              ConnectSocket
-                // йсдю опхмхлюрэ:     recvBuffer
-                // пюглеп асттепю      512   
+                result = recv(ClientSocket, recvBuffer, 512, 0);
+
                 if (result > 0)
                 {
                     std::cout << "Received " << result << " bytes" << std::endl;
@@ -212,7 +189,6 @@ namespace web {
             return 0;
         }
 
-        // гюйпшбюел янйер дкъ нропюбйх
         int CloseForSending() {
 
             result = shutdown(ClientSocket, SD_SEND);
@@ -232,13 +208,13 @@ namespace web {
     class client {
 
         WSADATA wsaDta;
-        ADDRINFO hints;                                      // срнвмемхъ янйерю
-        ADDRINFO* addrResult = NULL;                         // ярпсйрспю дкъ нрберю getaddrinfo()
-        SOCKET ClientSocket = INVALID_SOCKET;                // янгдюмхе янйерю х хмхжхюкхгюжхъ,         INVALID_SOCKET - мебепмши янйер 
+        ADDRINFO hints;                                      
+        ADDRINFO* addrResult = NULL;                         
+        SOCKET ClientSocket = INVALID_SOCKET;                
 
         int result = 0;
-        const char* sendBuffer = "Hello from Server";        // астеп дкъ нропюбйх яннаыемхъ мю яепбеп
-        char recvBuffer[512] = "";                           // астеп дкъ опх╗лю яннаыемхъ я яепбепю
+        const char* sendBuffer = "Hello from Server";        
+        char recvBuffer[512] = "";                           
 
     public:
         client(ADDRINFO* addrinfo) {
@@ -271,8 +247,8 @@ namespace web {
         int SetWSADATA() {
 
             int result;
-            result = WSAStartup(MAKEWORD(2, 2), &wsaDta); // оепедю╗л бепяхч янйернб WINDOWS: MAKEWORD(2, 2)
-            // х яяшкйс мю ярпсйрспс WsaData
+            result = WSAStartup(MAKEWORD(2, 2), &wsaDta);
+            
             if (result != 0)
             {
                 std::cout << "WSAStartup failed, result: " << result << std::endl;
@@ -283,11 +259,11 @@ namespace web {
 
         int CheckSocket(PCSTR port) {
 
-            result = getaddrinfo("localhost", port, &hints, &addrResult); // опнбепъер мюкхвхе сдюк╗ммнцн сгкю б яерх, х ошрюеряъ онярпнхрэ й мелс осрэ
+            result = getaddrinfo("localhost", port, &hints, &addrResult); 
             if (result != 0)
             {
                 std::cout << "getaddrinfo() failed, with error: " << result << std::endl;
-                WSACleanup(); // б яксвюе теикю нвхыюел
+                WSACleanup(); 
                 return 1;
             }
             return 0;
@@ -295,12 +271,8 @@ namespace web {
 
         int InitConnection() {
 
-            // хмхжхюкхгюжхъ янйер янедхмемхъ
-            ClientSocket = socket(addrResult->ai_family, addrResult->ai_socktype, addrResult->ai_protocol); // опхмхлюер:
-            // яелеиярбн опнрнйнкю: addrResult->ai_family
-            // рхо янйерю:       addrResult->ai_socktype
-            // рхо опнрнйнкю:    addrResult->ai_protocol
-            // опбепйю янгдюмхъ янйерю
+            ClientSocket = socket(addrResult->ai_family, addrResult->ai_socktype, addrResult->ai_protocol);
+
             if (ClientSocket == INVALID_SOCKET)
             {
                 std::cout << "Socket creation failed" << std::endl;
@@ -314,16 +286,10 @@ namespace web {
 
         int CheckConnection() {
 
-            // сярюмнбйю янедхмемхъ
-            result = connect(ClientSocket, addrResult->ai_addr, (int)addrResult->ai_addrlen); // опхмхлюер:
-            // янгдюммши янйер:         ConnectSocket
-            // юдпея дкъ янедхмемхъ:    addrResult->ai_addr
-            // дкхмю хлемх юдпеяю:      addrResult->ai_addrlen
+            result = connect(ClientSocket, addrResult->ai_addr, (int)addrResult->ai_addrlen); 
 
-            // опнбепйю янедхмемхъ
             if (result == SOCKET_ERROR)
             {
-                // б яксвюе ньхайх янедхмемхъ нябнанфдюел пеяспяш
                 std::cout << "Connection error!" << std::endl;
                 ClientSocket = INVALID_SOCKET;
                 ReleaseResources(ClientSocket);
@@ -334,13 +300,8 @@ namespace web {
 
         int Send() {
 
-            // нропюбйю яннаыемхъ мю яепбеп
-            result = send(ClientSocket, sendBuffer, (int)strlen(sendBuffer), 0); // опхмхлюер:
-            // янйер янедхмемхъ:     ConnectSocket
-            // яннаыемхе:            sendBuffer
-            // дкхмю яннаыемхъ:      (int)strlen(sendBuffer)
+            result = send(ClientSocket, sendBuffer, (int)strlen(sendBuffer), 0);
 
-            // бнгбпюыюер йнкхвеярбн оепедюммшу дюммшу кхан SOCKET_ERROR
             if (result == SOCKET_ERROR)
             {
                 std::cout << "Send failed, error:" << result << std::endl;
@@ -354,11 +315,8 @@ namespace web {
 
         int CloseSocket() {
 
-            // бшйкчвемхе янйерю
-            result = shutdown(ClientSocket, SD_SEND); // опхмхлюер:
-            // янйер:                    ConnectSocket
-            // оюпюлерп бшйкчвемхъ:      SD_SEND         - бшйкчвемхе мю оепедювс
-            // б яксвюе ньхайх нябнанфдюел бя╗ х бшундхл 
+            result = shutdown(ClientSocket, SD_SEND);
+
             if (result == SOCKET_ERROR)
             {
                 std::cout << "Shutdown error: " << result << std::endl;
@@ -372,16 +330,12 @@ namespace web {
 
         int GetUpdates() {
 
-            // опх╗л дюммшу дн гюйпшрхъ янедхмемхъ
             do
             {
-                // нвхыюел астеп дкъ опх╗лю
                 ZeroMemory(recvBuffer, 512);
 
-                result = recv(ClientSocket, recvBuffer, 512, 0); // опхмхлюер:
-                // янйер:              ConnectSocket
-                // йсдю опхмхлюрэ:     recvBuffer
-                // пюглеп асттепю      520   
+                result = recv(ClientSocket, recvBuffer, 512, 0);
+
                 if (result > 0)
                 {
                     std::cout << "Received " << result << " bytes" << std::endl;
